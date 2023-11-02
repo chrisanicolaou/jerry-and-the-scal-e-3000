a081bed9a0dedaf4c10b24466c523e6621422721
Christos Nicolaou <chris.anicolaou@yahoo.co.uk>
2023-11-02 22:40:50 +0000

    Add variables to player jump

--- src/player/Player.cs
+++ src/player/Player.cs
@@ -5,8 +5,13 @@ public class Player : KinematicBody2D
 {
     [Export] private float _speed = 1;
     [Export] private float _jumpSpeed = 1;
+    [Export] private float _jumpFloatyness = 0.1f;
+    [Export] private float _gravityMultiplier = 1;
+    [Export] private float _elongatedJumpMultiplier = 25;
+    [Export] private float _snappyFallMultiplier = 25;
     private Vector2 _velocity;
     private float _gravity = Convert.ToInt32(ProjectSettings.GetSetting("physics/2d/default_gravity"));
+    private float _timeInAir;
     public override void _PhysicsProcess(float delta)
     {
         var direction = Input.GetAxis("left", "right");
@@ -16,12 +21,22 @@ public class Player : KinematicBody2D
 
         if (!IsOnFloor())
         {
-            _velocity.y += _gravity * delta;
+            _timeInAir += delta;
+            _velocity.y += _gravity * _gravityMultiplier * delta;
+            if (_velocity.y < 0)
+            {
+                if (Input.IsActionPressed("jump")) _velocity.y -= _elongatedJumpMultiplier * delta;
+                _velocity.y += (1 / _jumpFloatyness) * _timeInAir;
+            }
+            else
+            {
+                _velocity.y += _snappyFallMultiplier * delta;
+            }
         }
-
-        if (IsOnFloor())
+        else
         {
-            _velocity.y = Input.IsActionJustPressed("jump") ? -_jumpSpeed : 0;
+            _timeInAir = 0;
+            _velocity.y = Input.IsActionPressed("jump") ? -_jumpSpeed : 0;
         }
     }
 }
