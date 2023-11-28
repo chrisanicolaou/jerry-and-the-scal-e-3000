333289b01453bc61e51a7574f78259de2a3fc547
Christos Nicolaou <chris.anicolaou@yahoo.co.uk>
2023-11-28 00:27:38 +0000

    Add key sfx

--- src/items/interactables/key/Key.cs
+++ src/items/interactables/key/Key.cs
@@ -1,14 +1,19 @@
 using Godot;
 using System;
+using ChiciStudios.GithubGameJam2023.Common.Audio;
 
 public class Key : RigidBody2D
 {
     [Signal] public delegate void KeyFound();
+
+    [Export] private AudioStream _keySfx;
     [Export] private NodePath _interactionAreaPath;
     protected InteractionArea InteractionArea { get; private set; }
+    private AudioManager _audioManager;
 
     public override void _Ready()
     {
+        _audioManager = GetNode<AudioManager>("/root/AudioManager");
         InteractionArea = GetNode<InteractionArea>(_interactionAreaPath);
         InteractionArea.Connect(nameof(InteractionArea.InteractionAreaActivated), this, nameof(OnInteractionAreaActivated));
     }
@@ -16,5 +21,6 @@ public class Key : RigidBody2D
     public virtual void OnInteractionAreaActivated()
     {
         EmitSignal(nameof(KeyFound));
+        _audioManager.PlaySfx(_keySfx);
     }
 }
